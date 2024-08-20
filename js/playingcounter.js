695fc6f6cad08f9d51daeb58ab0b8544052a2480
Jon Lyttle <jon@jonlyttle.com>
2024-08-20 18:43:42 -0500

    Use roproxy for playing counter page

--- js/playingcounter.js
+++ js/playingcounter.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const fetchPlayerCount = () => {
-        fetch('https://games.roblox.com/v1/games?universeIds=1511649232,3937161490,4267929747,4981761600,5755992087')
+        fetch('https://games.roproxy.com/v1/games?universeIds=1511649232,3937161490,4267929747,4981761600,5755992087')
           .then(response => response.json())
           .then(data => {
             const totalPlaying = data.data.reduce((sum, game) => sum + game.playing, 0);
